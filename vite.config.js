a4466e1f11d4eaf4e9f0bbf3c5ea5371409d1322
Adam Alam <adamalam7@outlook.com>
2025-07-17 15:05:38 -0500

    Refactor deployment workflow and update Vite configuration for improved compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RTOTracker/",
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
 });
